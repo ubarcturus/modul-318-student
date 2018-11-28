54f7c3a67aea6214ba31205ceefb1b6b95655d3a
ubarcturus <ubarcturus@gmail.com>
2018-11-28 16:06:26 +0100

    2

--- SwissTransportUI/Form1.Designer.cs
+++ SwissTransportUI/Form1.Designer.cs
@@ -28,13 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.box_start = new System.Windows.Forms.ComboBox();
+            this.box_dest = new System.Windows.Forms.ComboBox();
+            this.lbl_from = new System.Windows.Forms.Label();
+            this.lbl_to = new System.Windows.Forms.Label();
+            this.btn_search = new System.Windows.Forms.Button();
+            this.lst_list = new System.Windows.Forms.ListBox();
+            this.txt_start = new System.Windows.Forms.TextBox();
+            this.txt_dest = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // box_start
+            // 
+            this.box_start.FormattingEnabled = true;
+            this.box_start.Location = new System.Drawing.Point(149, 64);
+            this.box_start.Name = "box_start";
+            this.box_start.Size = new System.Drawing.Size(206, 21);
+            this.box_start.TabIndex = 0;
+            // 
+            // box_dest
+            // 
+            this.box_dest.FormattingEnabled = true;
+            this.box_dest.Location = new System.Drawing.Point(149, 111);
+            this.box_dest.Name = "box_dest";
+            this.box_dest.Size = new System.Drawing.Size(206, 21);
+            this.box_dest.TabIndex = 1;
+            // 
+            // lbl_from
+            // 
+            this.lbl_from.AutoSize = true;
+            this.lbl_from.Location = new System.Drawing.Point(78, 48);
+            this.lbl_from.Name = "lbl_from";
+            this.lbl_from.Size = new System.Drawing.Size(26, 13);
+            this.lbl_from.TabIndex = 2;
+            this.lbl_from.Text = "Von";
+            // 
+            // lbl_to
+            // 
+            this.lbl_to.AutoSize = true;
+            this.lbl_to.Location = new System.Drawing.Point(78, 94);
+            this.lbl_to.Name = "lbl_to";
+            this.lbl_to.Size = new System.Drawing.Size(33, 13);
+            this.lbl_to.TabIndex = 3;
+            this.lbl_to.Text = "Nach";
+            // 
+            // btn_search
+            // 
+            this.btn_search.Location = new System.Drawing.Point(181, 138);
+            this.btn_search.Name = "btn_search";
+            this.btn_search.Size = new System.Drawing.Size(75, 23);
+            this.btn_search.TabIndex = 4;
+            this.btn_search.Text = "Suchen";
+            this.btn_search.UseVisualStyleBackColor = true;
+            this.btn_search.Click += new System.EventHandler(this.btn_search_Click);
+            // 
+            // lst_list
+            // 
+            this.lst_list.FormattingEnabled = true;
+            this.lst_list.Location = new System.Drawing.Point(81, 197);
+            this.lst_list.Name = "lst_list";
+            this.lst_list.Size = new System.Drawing.Size(274, 199);
+            this.lst_list.TabIndex = 5;
+            // 
+            // txt_start
+            // 
+            this.txt_start.Location = new System.Drawing.Point(149, 45);
+            this.txt_start.Name = "txt_start";
+            this.txt_start.Size = new System.Drawing.Size(206, 20);
+            this.txt_start.TabIndex = 6;
+            this.txt_start.TextChanged += new System.EventHandler(this.txt_start_TextChanged);
+            // 
+            // txt_dest
+            // 
+            this.txt_dest.Location = new System.Drawing.Point(149, 91);
+            this.txt_dest.Name = "txt_dest";
+            this.txt_dest.Size = new System.Drawing.Size(206, 20);
+            this.txt_dest.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(445, 450);
+            this.Controls.Add(this.txt_dest);
+            this.Controls.Add(this.txt_start);
+            this.Controls.Add(this.lst_list);
+            this.Controls.Add(this.btn_search);
+            this.Controls.Add(this.lbl_to);
+            this.Controls.Add(this.lbl_from);
+            this.Controls.Add(this.box_dest);
+            this.Controls.Add(this.box_start);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox box_start;
+        private System.Windows.Forms.ComboBox box_dest;
+        private System.Windows.Forms.Label lbl_from;
+        private System.Windows.Forms.Label lbl_to;
+        private System.Windows.Forms.Button btn_search;
+        private System.Windows.Forms.ListBox lst_list;
+        private System.Windows.Forms.TextBox txt_start;
+        private System.Windows.Forms.TextBox txt_dest;
     }
 }
 
--- SwissTransportUI/Form1.cs
+++ SwissTransportUI/Form1.cs
@@ -7,14 +7,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SwissTransport;
 
 namespace SwissTransportUI
 {
     public partial class Form1 : Form
     {
+        Transport Transport = new Transport();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btn_search_Click(object sender, EventArgs e)
+        {
+            Connections connections = Transport.GetConnections(txt_start.Text, txt_dest.Text);
+            lst_list.Items.Clear();
+            foreach (var item in connections.ConnectionList)
+            {
+                lst_list.Items.Add(item.Duration + item.From.Station.Name + item.To.Station.Name);
+            }
+        }
+
+        private void txt_start_TextChanged(object sender, EventArgs e)
+        {
+            Stations stations = Transport.GetStations(txt_start.Text);
+            box_start.Items.Clear();
+            foreach (var item in stations.StationList)
+            {
+                if (item.Name != null)
+                {
+                    box_start.Items.Add(item.Name);
+                }
+            }
         }
     }
 }
